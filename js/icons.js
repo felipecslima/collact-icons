908bed3fcaf00bd8c63c03995192773e45935cf3
Felipe Lima <felipelima@MacBook-Pro-de-Felipe.local>
2017-06-30 18:57:15 -0300

    Add icons

---
+++ js/icons.js
@@ -0,0 +1,22 @@
+"use strict";
+
+angular.module('collact-iconsSvg', [])
+        .config(['$mdIconProvider', function ($mdIconProvider) {
+                $mdIconProvider
+                        .icon('cl-auto-pilot', 'https://s3-sa-east-1.amazonaws.com/res.collact.com.br/imgs/icons/cl-auto-pilot.svg', 24)
+                        .icon('cl-bday', 'https://s3-sa-east-1.amazonaws.com/res.collact.com.br/imgs/icons/cl-bday.svg', 24)
+                        .icon('cl-business', 'https://s3-sa-east-1.amazonaws.com/res.collact.com.br/imgs/icons/cl-business.svg', 24)
+                        .icon('cl-campaign', 'https://s3-sa-east-1.amazonaws.com/res.collact.com.br/imgs/icons/cl-campaign.svg', 24)
+                        .icon('cl-config', 'https://s3-sa-east-1.amazonaws.com/res.collact.com.br/imgs/icons/cl-config.svg', 24)
+                        .icon('cl-diamond', 'https://s3-sa-east-1.amazonaws.com/res.collact.com.br/imgs/icons/cl-diamond.svg', 24)
+                        .icon('cl-doubt', 'https://s3-sa-east-1.amazonaws.com/res.collact.com.br/imgs/icons/cl-doubt.svg', 24)
+                        .icon('cl-feature', 'https://s3-sa-east-1.amazonaws.com/res.collact.com.br/imgs/icons/cl-feature.svg', 24)
+                        .icon('cl-ghost', 'https://s3-sa-east-1.amazonaws.com/res.collact.com.br/imgs/icons/cl-ghost.svg', 24)
+                        .icon('cl-ghost-outline', 'https://s3-sa-east-1.amazonaws.com/res.collact.com.br/imgs/icons/cl-ghost-outline.svg', 24)
+                        .icon('cl-home', 'https://s3-sa-east-1.amazonaws.com/res.collact.com.br/imgs/icons/cl-home.svg', 24)
+                        .icon('cl-sirens', 'https://s3-sa-east-1.amazonaws.com/res.collact.com.br/imgs/icons/cl-sirens.svg', 24)
+                        .icon('cl-tablet', 'https://s3-sa-east-1.amazonaws.com/res.collact.com.br/imgs/icons/cl-tablet.svg', 24)
+                        .icon('cl-user', 'https://s3-sa-east-1.amazonaws.com/res.collact.com.br/imgs/icons/cl-user.svg', 24)
+                        ;
+
+            }]);
